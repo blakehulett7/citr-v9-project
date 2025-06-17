ddd80c8cb7d66eb2d2edb5f43fbea7f309946d38
blakehulett7 <blake.hulett7@gmail.com>
2025-06-17 10:51:35 -0500

    prettier formatting

--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -1,32 +1,29 @@
 function Pizza(args) {
-    return React.createElement("div", {}, [
-        React.createElement("h2", {}, args.name),
-        React.createElement("p", {}, args.description)
-    ])
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, args.name),
+    React.createElement("p", {}, args.description),
+  ]);
 }
 
 function App() {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Padre Gino's"),
-            React.createElement(Pizza, {
-                name: "Cheeze Pizza",
-                description: "Mozzarella, ColbyJack, Cheddar, American"
-            }),
-            React.createElement(Pizza, {
-                name: "Pepperoni",
-                description: "A pepperoni pizza"
-            }),
-            React.createElement(Pizza, {
-                name: "Sausage",
-                description: "delicious"
-            }),
-        ]
-    )
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+
+    React.createElement(Pizza, {
+      name: "Cheeze Pizza",
+      description: "Mozzarella, ColbyJack, Cheddar, American",
+    }),
+    React.createElement(Pizza, {
+      name: "Pepperoni",
+      description: "A pepperoni pizza",
+    }),
+    React.createElement(Pizza, {
+      name: "Sausage",
+      description: "delicious",
+    }),
+  ]);
 }
 
-const container = document.getElementById("root")
-const root = ReactDOM.createRoot(container)
-root.render(React.createElement(App))
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
