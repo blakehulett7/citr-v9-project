ce7523839467cc25260acc4156d5df62d16373d1
blakehulett7 <blake.hulett7@gmail.com>
2025-09-15 16:06:11 -0500

    adds prettier config uses it on inner circle

--- inner-circle/src/App.js
+++ inner-circle/src/App.js
@@ -5,30 +5,29 @@ function App() {
         React.createElement("h1", {}, "DW Inner Circle"),
         React.createElement(Section, {
             name: "Get Started",
-            description: "New to the Inner Circle? Start here to get up and running"
+            description:
+                "New to the Inner Circle? Start here to get up and running",
         }),
         React.createElement(Section, {
             name: "DW Labs",
-            description: "Test and shape our digital products, consumer goods, and platform features"
+            description:
+                "Test and shape our digital products, consumer goods, and platform features",
         }),
         React.createElement(Section, {
             name: "DW Studio",
-            description: "Preview exclusive content and help shape our entertainment offerings"
+            description:
+                "Preview exclusive content and help shape our entertainment offerings",
         }),
-    )
+    );
 }
 
 function Section(props) {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h2", {}, props.name),
-            React.createElement("p", {}, props.description),
-        ]
-    )
+    return React.createElement("div", {}, [
+        React.createElement("h2", {}, props.name),
+        React.createElement("p", {}, props.description),
+    ]);
 }
 
-const container = document.querySelector("#root")
-const root = ReactDOM.createRoot(container)
-root.render(React.createElement(App))
+const container = document.querySelector("#root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
