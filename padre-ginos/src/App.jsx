dbe18d02d56dee29f1d794409e7f41d9571f77a2
blakehulett7 <blake.hulett7@gmail.com>
2025-09-16 18:59:50 -0500

    adds images from the api

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -5,9 +5,21 @@ function App() {
     return (
         <div>
             <h1>Padre Gino's Pizza - Order Now</h1>
-            <Pizza name="Cheese" description="Good" />
-            <Pizza name="Pepperoni" description="Better" />
-            <Pizza name="Sausage" description="Best" />
+            <Pizza
+                name="Cheese"
+                description="Good"
+                image="/public/pizzas/four_cheese.webp"
+            />
+            <Pizza
+                name="Pepperoni"
+                description="Better"
+                image="/public/pizzas/pepperoni.webp"
+            />
+            <Pizza
+                name="Meat"
+                description="Best"
+                image="/public/pizzas/big_meat.webp"
+            />
         </div>
     );
 }
