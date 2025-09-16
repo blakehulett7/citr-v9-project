d7f63c5089e0231237df3e39ecd499987165ab0d
blakehulett7 <blake.hulett7@gmail.com>
2025-09-16 18:34:25 -0500

    adds pizza component

---
+++ padre-ginos/src/Pizza.jsx
@@ -0,0 +1,8 @@
+export default function Pizza(props) {
+    return (
+        <div className="pizza">
+            <h1>{props.name}</h1>
+            <p>{props.description}</p>
+        </div>
+    );
+}
