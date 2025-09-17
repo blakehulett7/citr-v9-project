d21e631c1f25dfe3986e23e41774431e72be6dfd
blakehulett7 <blake.hulett7@gmail.com>
2025-09-17 08:14:09 -0500

    now we are using jsx.. proper reacting

---
+++ inner-circle/src/App.jsx
@@ -0,0 +1,26 @@
+import { createRoot } from "react-dom/client";
+import Category from "./Category.jsx";
+
+function App() {
+    return (
+        <div>
+            <h1>DW Inner Circle</h1>
+            <Category
+                name="Get Started"
+                description="New to the Inner Circle? Start here to get up and running"
+            />
+            <Category
+                name="DW Labs"
+                description="Test and shape our digital products, consumer goods, and platform features"
+            />
+            <Category
+                name="DW Studio"
+                description="Preview exclusive content and help shape our entertainment offerings"
+            />
+        </div>
+    );
+}
+
+const container = document.querySelector("#root");
+const root = createRoot(container);
+root.render(<App />);
---
+++ inner-circle/src/Category.jsx
@@ -0,0 +1,8 @@
+export default function Category(props) {
+    return (
+        <div>
+            <h2>{props.name}</h2>
+            <p>{props.description}</p>
+        </div>
+    );
+}
