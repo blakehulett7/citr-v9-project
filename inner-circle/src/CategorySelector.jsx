b9fb895a7107767a8d840eb8592219230d2d73ba
blakehulett7 <blake.hulett7@gmail.com>
2025-09-18 12:56:11 -0500

    better styling

--- inner-circle/src/CategorySelector.jsx
+++ inner-circle/src/CategorySelector.jsx
@@ -5,29 +5,27 @@ export default function CategorySelector() {
 
     return (
         <form onChange={(e) => setSelectedCategory(e.target.value)}>
-            <div>
-                <input
-                    type="radio"
-                    name="selectedCategory"
-                    value="Getting Started"
-                    checked={selectedCategory == "Getting Started"}
-                />
-                Getting Started
-                <input
-                    type="radio"
-                    name="selectedCategory"
-                    value="Labs"
-                    checked={selectedCategory == "Labs"}
-                />
-                Labs
-                <input
-                    type="radio"
-                    name="selectedCategory"
-                    value="Studio"
-                    checked={selectedCategory == "Studio"}
-                />
-                Studio
-            </div>
+            <input
+                type="radio"
+                name="selectedCategory"
+                value="Getting Started"
+                checked={selectedCategory == "Getting Started"}
+            />
+            Getting Started
+            <input
+                type="radio"
+                name="selectedCategory"
+                value="Labs"
+                checked={selectedCategory == "Labs"}
+            />
+            Labs
+            <input
+                type="radio"
+                name="selectedCategory"
+                value="Studio"
+                checked={selectedCategory == "Studio"}
+            />
+            Studio
             <input type="submit" />
         </form>
     );
