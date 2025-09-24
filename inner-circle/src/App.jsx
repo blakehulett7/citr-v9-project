1b17a2f8ae65ed86d536cecc29d2dc7a2dddc5e3
blakehulett7 <blake.hulett7@gmail.com>
2025-09-24 13:09:32 -0500

    adding the context

--- inner-circle/src/App.jsx
+++ inner-circle/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
+import { UserContext } from "./contexts.jsx";
 import Category from "./Category.jsx";
 import CategorySelector from "./CategorySelector.jsx";
 import useCategories from "./useCategories.jsx";
@@ -7,6 +8,8 @@ import useCategories from "./useCategories.jsx";
 function App() {
     const categories = useCategories();
     const [category, setCategory] = useState([]);
+
+    const userHook = useState([]);
 
     useEffect(
         function () {
@@ -18,29 +21,31 @@ function App() {
     );
 
     return (
-        <div className="app">
-            <h1>DW Inner Circle</h1>
-            <CategorySelector
-                categories={categories}
-                setCategory={setCategory}
-            />
-            <div id="categories">
-                {!category ? (
-                    <span className="loader"></span>
-                ) : (
-                    category.map(function (category) {
-                        return (
-                            <Category
-                                key={category.name}
-                                name={category.name}
-                                description={category.description}
-                                image={category.image}
-                            />
-                        );
-                    })
-                )}
+        <UserContext.Provider value={userHook}>
+            <div className="app">
+                <h1>DW Inner Circle</h1>
+                <CategorySelector
+                    categories={categories}
+                    setCategory={setCategory}
+                />
+                <div id="categories">
+                    {!category ? (
+                        <span className="loader"></span>
+                    ) : (
+                        category.map(function (category) {
+                            return (
+                                <Category
+                                    key={category.name}
+                                    name={category.name}
+                                    description={category.description}
+                                    image={category.image}
+                                />
+                            );
+                        })
+                    )}
+                </div>
             </div>
-        </div>
+        </UserContext.Provider>
     );
 }
 
